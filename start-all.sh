baa1b9eeef81ac23873b6ad033e277b527376308
dettanym <dettanym@users.noreply.github.com>
2023-03-02 14:09:04 -0500

    fix: Comment out sitewhere installs (abandonware)

--- start-all.sh
+++ start-all.sh
@@ -12,13 +12,13 @@ kubectl apply -f deploy/kubernetes/manifests-jaeger && \
 #Wait for all the Sock Shop services to start:
 sleep 5 && kubectl get pods --namespace="sock-shop"
 
-#For sitewhere --- abandonware 
-#From https://github.com/sitewhere/sitewhere-k8s/tree/master/charts/sitewhere#add-sitewhere-helm 
-helm repo add sitewhere-repo https://sitewhere.io/helm-charts
-helm repo update
+# For sitewhere --- abandonware 
+# From https://github.com/sitewhere/sitewhere-k8s/tree/master/charts/sitewhere#add-sitewhere-helm 
+#helm repo add sitewhere-repo https://sitewhere.io/helm-charts
+#helm repo update
 #From https://github.com/sitewhere/sitewhere-k8s/tree/master/charts/sitewhere-infrastructure#installing-the-chart
-helm install sitewhere-infra-instance -n sitewhere-system --create-namespace sitewhere-repo/sitewhere-infrastructure 
-helm install sitewhere-instance -n sitewhere --create-namespace sitewhere-repo/sitewhere
+#helm install sitewhere-infra-instance -n sitewhere-system --create-namespace sitewhere-repo/sitewhere-infrastructure 
+#helm install sitewhere-instance -n sitewhere --create-namespace sitewhere-repo/sitewhere
 #Both comamnds threw up errors as the CRDs have an older version (v1beta versus more recent v1)
 #Error: INSTALLATION FAILED: failed to install CRD crds/040-Crd-kafka.yaml: resource mapping not found for name: 
 #"kafkas.kafka.strimzi.io" namespace: "" from "": no matches for kind "CustomResourceDefinition" in version "apiextensions.k8s.io/v1beta1"
