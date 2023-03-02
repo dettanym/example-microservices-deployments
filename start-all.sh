d4eeca6e64609ee4ae943b537e75654eef6f0b97
Miti Mazmudar <mitimazmudar@gmail.com>
2023-03-02 15:15:44 -0500

    fix: remove unnecessary cds

--- start-all.sh
+++ start-all.sh
@@ -1,6 +1,5 @@
 #For online boutique
-cd online-boutique && kubectl apply -f release/kubernetes-manifests.yaml 
-cd ../ 
+kubectl apply -f online-boutique/release/kubernetes-manifests.yaml
 
 #For pitstop: from https://github.com/EdwinVW/pitstop/wiki/Run%20the%20application%20on%20Kubernetes
 cd pitstop/src/k8s && ./start-all.sh --nomesh 
@@ -9,12 +8,11 @@ cd ../../../
 #For sockshop: from https://microservices-demo.github.io/deployment/kubernetes-minikube.html
 #Deploy the Sock Shop application on Minikube
 git apply --directory=sockshop patchfiles/sockshop-nodeports.patch
-cd sockshop && kubectl create -f deploy/kubernetes/manifests && \
+kubectl create -f sockshop/deploy/kubernetes/manifests
 #To start Opentracing run the following command after deploying the sock shop
-kubectl apply -f deploy/kubernetes/manifests-jaeger && \
+kubectl create -f sockshop/deploy/kubernetes/manifests-jaeger
 #Wait for all the Sock Shop services to start:
 sleep 5 && kubectl get pods --namespace="sock-shop"
-cd ../
 git apply -R --directory=sockshop patchfiles/sockshop-nodeports.patch
 
 # For sitewhere --- abandonware 
